2874c46f101f5927c5952ae4a37aa2bfe7931221
JBerger <JBerger@melco.com>
2017-04-06 14:55:06 -0600

    Added default mode which doesn't show a preview; and just prints.

--- main.cpp
+++ main.cpp
@@ -20,43 +20,59 @@
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
 
+  bool showPreview = argc > 2 && std::string(argv[1]) == "--preview"; 
+  size_t startPdf = showPreview ? 2 : 1;
+
   std::vector<std::shared_ptr<Poppler::Document>> docs;
-  for(size_t i = 1;i < argc;i++)
+  for(size_t i = startPdf;i < argc;i++)
 	  docs.emplace_back(Poppler::Document::load(argv[i]));
 
-  QPrintPreviewDialog *pd = new QPrintPreviewDialog();
-  
-  pd->connect(pd, &QPrintPreviewDialog::paintRequested,
-	      [&](QPrinter* printer) {
+  auto printFunction = [&](QPrinter* printer) {
 	  printer->setResolution(300);
 
-	auto resolution = printer->resolution();
-		QPainter painter;
-		if (! painter.begin(printer)) { // failed to open file
+	  auto resolution = printer->resolution();
+	  QPainter painter;
+	  if (!painter.begin(printer)) { // failed to open file
 		  qWarning("failed to open file, is it writable?");
 		  return;
-		}
-		bool isFirst = true;
-		for (auto& doc : docs) {
-			for (size_t i = 0;i < 1;i++) {
-				if (!isFirst)
-					printer->newPage();
-				isFirst = false; 
+	  }
+	  bool isFirst = true;
+	  for (auto& doc : docs) {
+		  for (size_t i = 0;i < 1;i++) {
+			  if (!isFirst)
+				  printer->newPage();
+			  isFirst = false;
 
-				std::unique_ptr<Poppler::Page> page(doc->page(i));
-				auto mult = 1.0;
-				auto image = page->renderToImage(resolution * mult, resolution * mult);
-				if (!image.isNull()) {
-					painter.drawImage(QRectF(0, 0, image.width() / mult, image.height() / mult), image,
-						QRectF(0, 0, image.width(), image.height()),
-						Qt::AutoColor | Qt::DiffuseAlphaDither | Qt::DiffuseDither | Qt::PreferDither
-					);
-				}
-			}
-		}
-	      });
-  pd->setAttribute(Qt::WA_DeleteOnClose);
-  pd->show();
+			  std::unique_ptr<Poppler::Page> page(doc->page(i));
+			  auto mult = 1.0;
+			  auto image = page->renderToImage(resolution * mult, resolution * mult);
+			  if (!image.isNull()) {
+				  painter.drawImage(QRectF(0, 0, image.width() / mult, image.height() / mult), image,
+					  QRectF(0, 0, image.width(), image.height()),
+					  Qt::AutoColor | Qt::DiffuseAlphaDither | Qt::DiffuseDither | Qt::PreferDither
+				  );
+			  }
+		  }
+	  }
+  };
 
-  return app.exec();
+  if (showPreview) {
+	  QPrintPreviewDialog *pd = new QPrintPreviewDialog();
+
+	  pd->connect(pd, &QPrintPreviewDialog::paintRequested, printFunction);
+	  pd->setAttribute(Qt::WA_DeleteOnClose);
+	  pd->show();
+	  
+	  return app.exec();
+  }
+  else {
+	  QPrinter printer(QPrinter::HighResolution); 
+	  
+	  QPrintDialog* pd = new QPrintDialog(&printer);	  
+	  if (pd->exec() == QDialog::Accepted) {
+		  printFunction(&printer);
+	  }	  
+  }
+
+  return 0;
 }
